cd234fa4b72c2a575ddb83211a548cff600169f7
Yves Yuen <yves.j.yuen@gmail.com>
2014-12-21 03:47:20 -0800

    delete debugging set up and route

--- app/routes.php
+++ app/routes.php
@@ -11,9 +11,6 @@
 |
 */
 
-#Final Route
-
-
 Route::get('/', 'ListController@getIndex');
 Route::get('/item/delete', 'ItemController@getDelete');
 Route::post('/item/delete', 'ItemController@postDelete');
@@ -26,81 +23,5 @@ Route::post('/signup', 'UserController@postSignup');
 Route::post('/login', 'UserController@postLogin');
 Route::get('/login', 'UserController@getLogin');
 Route::get('/logout', 'UserController@getLogout');
-
-
-
-# Practice debug route
-Route::get('/debug', function() {
-
-    echo '<pre>';
-
-    echo '<h1>environment.php</h1>';
-    $path   = base_path().'/environment.php';
-
-    try {
-        $contents = 'Contents: '.File::getRequire($path);
-        $exists = 'Yes';
-    }
-    catch (Exception $e) {
-        $exists = 'No. Defaulting to `production`';
-        $contents = '';
-    }
-
-    echo "Checking for: ".$path.'<br>';
-    echo 'Exists: '.$exists.'<br>';
-    echo $contents;
-    echo '<br>';
-
-    echo '<h1>Environment</h1>';
-    echo App::environment().'</h1>';
-
-    echo '<h1>Debugging?</h1>';
-    if(Config::get('app.debug')) echo "Yes"; else echo "No";
-
-    echo '<h1>Database Config</h1>';
-    print_r(Config::get('database.connections.mysql'));
-
-    echo '<h1>Test Database Connection</h1>';
-    try {
-        $results = DB::select('SHOW DATABASES;');
-        echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
-        echo "<br><br>Your Databases:<br><br>";
-        print_r($results);
-    } 
-    catch (Exception $e) {
-        echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
-    }
-
-    echo '</pre>';
-
-});
-
-
-Route::get('/testconnection', 
-    array(
-        'before' => 'auth', 
-        function() {
-            echo 'Congratz !';
-        }
-    )
-);
-
-Route::get('/test', function(){
-
-//     $list = PantryList::where('user_id','=',Auth::id())->first();
-
-//     echo $list.'<br>'; 
-//     echo $list->id.'<br>'; 
-//     // $items = ListContent::Where('pantry_list_id','=',1);
-//     $items = ListContent::Where('pantry_list_id','=',$list->id)->get();
-// //$list->id
-//     foreach ($items as $item) {
-//          echo $item->item_id.'<br>';
-//     }
-
-//     $itemCount = Item::where('default','=',TRUE)->count();
-//     echo $itemCount;
-
-        return View::make('item_edit');
 
 });
